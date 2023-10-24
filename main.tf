9852984420b1725851830026040b4b425ae31ec2
Henrylle Maia <henryllemaia@gmail.com>
2023-10-24 16:42:47 -0300

    fontes da aula 06

--- main.tf
+++ main.tf
@@ -15,14 +15,36 @@ provider "aws" {
   profile = "bia"
 }
 
+resource "aws_security_group" "bia_dev" {
+  name        = "bia-dev-tf"
+  description = "Regra para a instancia de trabalho bia-dev com tf"
+  vpc_id      = "vpc-07400a2ed02c4aba4"
+
+  ingress {
+    description = "Liberado 3001 para o mundo"
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_instance" "bia-dev" {
   ami           = "ami-02f3f602d23f1659d"
   instance_type = "t3.micro"
   tags = {
     ambiente = "dev"
-    Name     = "bia-dev"
+    Name     = var.instance_name
   }
-  vpc_security_group_ids = ["sg-03b7ebc50d9cb400c"]
+  vpc_security_group_ids = [aws_security_group.bia_dev.id]
   root_block_device {
     volume_size = 10
   }
