d6812dcdc03f9b209c569644e0b8e4d04debdfca
Henrylle Maia <henryllemaia@gmail.com>
2023-11-01 15:17:16 -0300

    fontes da aula

--- main.tf
+++ main.tf
@@ -16,12 +16,12 @@ provider "aws" {
 }
 
 resource "aws_security_group" "bia_dev" {
-  name        = "bia-dev-tf"
-  description = "Regra para a instancia de trabalho bia-dev com tf"
+  name        = "bia-dev"
+  description = "aceso da bia dev"
   vpc_id      = "vpc-07400a2ed02c4aba4"
 
   ingress {
-    description = "Liberado 3002 para o mundo"
+    description = "acesso geral"
     from_port   = 3001
     to_port     = 3001
     protocol    = "tcp"
@@ -48,4 +48,39 @@ resource "aws_instance" "bia-dev" {
   root_block_device {
     volume_size = 12
   }
+  iam_instance_profile = aws_iam_instance_profile.role_acesso_ssm.name
+  user_data            = <<EOF
+  #!/bin/bash
+
+#Instalar Docker e Git
+sudo yum update -y
+sudo yum install git -y
+sudo yum install docker -y
+sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker ssm-user
+id ec2-user ssm-user
+sudo newgrp docker
+
+
+#Instalar docker-compose
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+
+#Ativar docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+#Adicionar swap
+sudo dd if=/dev/zero of=/swapfile bs=128M count=32
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+
+#Adicionar node 18 e npm
+sudo yum install https://rpm.nodesource.com/pub_18.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
+sudo yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
+
+  EOF
 }
